2f5af20a032a8f1e4d6b3c0a5a9d20d62fa58938
hanhehun1 <khanhtqgcd18464@fpt.edu.vn>
2021-02-23 13:33:47 +0700

    + Add topic view

---
+++ views/topic/inprogress.php
@@ -0,0 +1,31 @@
+<div class="col-md-12">
+	<div class="card">
+		<div class="card-header">List Inprogress Topic</div>
+		<div class="card-body">
+			<table class="table table-responsive-sm table-bordered">
+				<thead class="thead-light">
+					<tr>
+						<th class="text-center align-middle" style="width:5%">ID</th>
+						<th class="text-center align-middle" style="width:50%">Name</th>
+						<th class="text-center align-middle" style="width:45%">Course Name</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($topics as $topic) { ?>
+						<tr>
+							<td class="text-center align-middle">
+								<div><?= $topic->id; ?> <div>
+							</td>
+							<td class="align-middle">
+								<div><?= $topic->name; ?><div>
+							</td>
+							<td class="text-center align-middle">
+								<div><?= $topic->courseName; ?><div>
+							</td>
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
---
+++ views/topic/show.php
@@ -0,0 +1,59 @@
+<?php
+$isUpdate = true;
+if (!isset($topic->id)) {
+  $isUpdate = false;
+}
+if ($isUpdate) $form = "./?controller=topic&action=update";
+else $form = "./?controller=topic&action=add";
+?>
+<div class="col-12">
+  <div class="card">
+    <div class="card-header">
+      <h5><?= $isUpdate ? "<strong>Update</strong>" : "<strong>Add</strong>"; ?> Topic</h5>
+    </div>
+    <div class="card-block table-border-style">
+      <form id="frmChange" class="form-horizontal" action="<?= $form ?>" method="post" enctype="multipart/form-data">
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">ID</label>
+          <div class="col-md-9">
+            <input class="form-control" id="text-input" type="text" name="id" value="<?= $topic->id ?>" disabled>
+            <input type="hidden" name="id" value="<?= $topic->id ?>">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">Name</label>
+          <div class="col-md-9">
+            <input class="form-control" id="text-input" type="text" name="name" value="<?= $topic->name ?>">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">Trainer</label>
+          <div class="col-md-9">
+            <select class="form-control" name="trainerId">
+              <?php foreach ($trainers as $trainer) { ?>
+                <option value="<?= $trainer->id ?>" <?= $topic->trainerId == $trainer->id ? "selected" : "" ?>><?= $trainer->name ?></option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">Course</label>
+          <div class="col-md-9">
+            <select class="form-control" name="courseId">
+              <?php foreach ($courses as $course) { ?>
+                <option value="<?= $course->id ?>" <?= $topic->courseId == $course->id ? "selected" : "" ?>><?= $course->name ?></option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
+      </form>
+
+    </div>
+    <div class="card-footer">
+      <button class="btn btn-sm btn-primary" type="submit" onclick="$('#frmChange').submit();">
+        <i class="fa fa-dot-circle-o"></i> Save changes</button>
+      <a href="./?controller=topic"><button class="btn btn-sm btn-danger" type="reset">
+          <i class="fa fa-ban"></i> Cancel</button><a>
+    </div>
+  </div>
+</div>
