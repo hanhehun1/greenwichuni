1d0035e71d49c49466e1cb5fef91786d5ca70bac
hanhehun1 <khanhtqgcd18464@fpt.edu.vn>
2021-02-23 13:32:05 +0700

    + Add pages view

---
+++ views/pages/error.php
@@ -0,0 +1,5 @@
+<div class="col-md-12">
+  <div class="card">
+    <div class="card-header">There was an error!</div>
+  </div>
+</div>
---
+++ views/pages/home.php
@@ -0,0 +1,16 @@
+<div class="col-sm-12">
+  <!-- Bootstrap tab card start -->
+  <div class="card">
+    <div class="card-header">
+      <h5>Welcome</h5>
+    </div>
+    <div class="card-block">
+      <!-- Row start -->
+      <div class="row">
+        
+      </div>
+      <!-- Row end -->
+    </div>
+  </div>
+  <!-- Bootstrap tab card end -->
+</div>
