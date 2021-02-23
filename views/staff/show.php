a0e0f2d6c0ba6d46b34375f30164491da92173c2
hanhehun1 <khanhtqgcd18464@fpt.edu.vn>
2021-02-23 13:33:18 +0700

    + Add staff view

---
+++ views/staff/show.php
@@ -0,0 +1,50 @@
+<?php
+$isUpdate = true;
+if (!isset($staff->id)) {
+  $isUpdate = false;
+}
+if ($isUpdate) $form = "./?controller=staff&action=update";
+else $form = "./?controller=staff&action=add";
+?>
+<div class="col-12">
+  <div class="card">
+    <div class="card-header">
+      <h5><?= $isUpdate ? "<strong>Update</strong>" : "<strong>Add</strong>"; ?> Staff</h5>
+    </div>
+    <div class="card-block table-border-style">
+      <form id="frmChange" class="form-horizontal" action="<?= $form ?>" method="post" enctype="multipart/form-data">
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">ID</label>
+          <div class="col-md-9">
+            <input class="form-control" id="text-input" type="text" name="id" value="<?= $staff->id ?>" disabled>
+            <input type="hidden" name="id" value="<?= $staff->id ?>">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">Name</label>
+          <div class="col-md-9">
+            <input class="form-control" id="text-input" type="text" name="name" value="<?= $staff->name ?>">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">Email</label>
+          <div class="col-md-9">
+            <input class="form-control" id="text-input" type="text" name="email" value="<?= $staff->email ?>">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-3 col-form-label" for="text-input">Password</label>
+          <div class="col-md-9">
+            <input class="form-control" id="text-input" type="password" name="password" value="<?= $staff->password ?>">
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="card-footer">
+      <button class="btn btn-sm btn-primary" type="submit" onclick="$('#frmChange').submit();">
+        <i class="fa fa-dot-circle-o"></i> Save changes</button>
+      <a href="./?controller=staff"><button class="btn btn-sm btn-danger" type="reset">
+          <i class="fa fa-ban"></i> Cancel</button><a>
+    </div>
+  </div>
+</div>
